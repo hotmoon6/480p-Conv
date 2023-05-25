20cc45376bdb30eabe0461b8749cc71a288e33a2
Hotmoon <aravind1333.av@gmail.com>
2023-05-25 18:31:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,54 @@
-from flask import Flask, render_template, request, jsonify
-import requests
-import subprocess
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
 import os
+import ffmpeg
+import requests
 
 app = Flask(__name__)
+app.config['CONVERTED_FOLDER'] = 'converted'
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    video_url = request.json['videoUrl']
-    video_filename = 'input.mp4'
-    converted_filename = 'output.mp4'
+    # Get the video URL from the form
+    video_url = request.form.get('video_url')
 
-    try:
-        # Download the video
-        response = requests.get(video_url)
-        with open(video_filename, 'wb') as file:
-            file.write(response.content)
+    # Check if a video URL was provided
+    if not video_url:
+        return render_template('index.html', message='No video URL provided')
 
-        # Convert the video
-        subprocess.call(['ffmpeg', '-i', video_filename, '-vf', 'scale=-2:480', '-c:a', 'copy', converted_filename])
+    # Download the video from the URL
+    response = requests.get(video_url)
+    if response.status_code != 200:
+        return render_template('index.html', message='Failed to download video from the provided URL')
 
-        # Return the download link
-        download_url = '/' + converted_filename
-        return jsonify({'downloadUrl': download_url})
+    # Save the downloaded video to a file
+    filename = secure_filename('video.mp4')
+    video_path = os.path.join(app.config['CONVERTED_FOLDER'], filename)
+    with open(video_path, 'wb') as file:
+        file.write(response.content)
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    # Convert the video to 480p
+    converted_filename = f"converted_{filename}"
+    converted_path = os.path.join(app.config['CONVERTED_FOLDER'], converted_filename)
+    ffmpeg.input(video_path).output(converted_path, vf='scale=854:480').run()
+
+    # Provide the download link to the converted video
+    return render_template('index.html', converted_file=converted_filename)
+
+@app.route('/download/<path:filename>')
+def download(filename):
+    converted_path = os.path.join(app.config['CONVERTED_FOLDER'], filename)
+
+    # Check if the file exists
+    if not os.path.isfile(converted_path):
+        return render_template('index.html', message='File not found')
+
+    # Send the file for download
+    return send_file(converted_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
